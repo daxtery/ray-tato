e65c1c67d491fed80d785e8b122e15dd4432db6a
David Pinto <devdavidlp@gmail.com>
2026-01-06 23:00:15 +0000

    very basic waves

--- src/main.c
+++ src/main.c
@@ -5,10 +5,11 @@
 
 typedef struct
 {
+    Rectangle *player;
     Rectangle bounds;
 } PlayerControllerOptions;
 
-void player_move(Rectangle *player, const PlayerControllerOptions options, const Vector2 direction)
+static void player_move(PlayerControllerOptions *this, const Vector2 direction)
 {
     if (direction.x == 0 && direction.y == 0)
     {
@@ -17,24 +18,84 @@ void player_move(Rectangle *player, const PlayerControllerOptions options, const
 
     Vector2 new_position = Vector2Add(
         (Vector2){
-            .x = player->x,
-            .y = player->y,
+            .x = this->player->x,
+            .y = this->player->y,
         },
         direction);
 
     new_position = Vector2Clamp(new_position,
                                 (Vector2){
-                                    .x = options.bounds.x,
-                                    .y = options.bounds.y,
+                                    .x = this->bounds.x,
+                                    .y = this->bounds.y,
                                 },
                                 (Vector2){
-                                    .x = options.bounds.x + options.bounds.width - player->width,
-                                    .y = options.bounds.y + options.bounds.height - player->height,
+                                    .x = this->bounds.x + this->bounds.width - this->player->width,
+                                    .y = this->bounds.y + this->bounds.height - this->player->height,
                                 });
 
-    player->x = new_position.x;
-    player->y = new_position.y;
+    this->player->x = new_position.x;
+    this->player->y = new_position.y;
 }
+
+#define Entity_Id uint32_t
+typedef struct
+{
+    size_t health;
+} Enemy;
+
+typedef struct
+{
+    size_t count;
+    Enemy *items;
+} AWave;
+
+#define make_wave(its)                                                                                                 \
+    {                                                                                                                  \
+        .items = (its),                                                                                                \
+        .count = NOB_ARRAY_LEN((its)),                                                                                 \
+    }
+
+typedef struct
+{
+    AWave *items;
+    size_t count;
+    size_t capacity;
+    size_t current;
+} Waves;
+
+bool is_wave_over(AWave *wave)
+{
+    nob_da_foreach(Enemy, it, wave)
+    {
+        if (it->health > 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static Enemy wave_1_enemies[] = {
+    {
+        .health = 1,
+    },
+};
+
+static Enemy wave_2_enemies[] = {
+    {.health = 1},
+    {
+        .health = 1,
+    },
+};
+
+static Enemy wave_3_enemies[] = {
+    {
+        .health = 1,
+    },
+    {
+        .health = 1,
+    },
+};
 
 int main(void)
 {
@@ -56,17 +117,27 @@ int main(void)
                 .height = 800,
                 .width = 1000,
             },
+        .player = &player,
     };
 
-    Camera2D camera = {0};
-    camera.target = (Vector2){player.x + player.width / 2.0f, player.y + player.height / 2.0f};
-    camera.offset = (Vector2){renderWidth / 2.0f, renderHeight / 2.0f};
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
+    Camera2D camera = {
+        .target = (Vector2){player.x + player.width / 2.0f, player.y + player.height / 2.0f},
+        .offset = (Vector2){renderWidth / 2.0f, renderHeight / 2.0f},
+        .rotation = 0.0f,
+        .zoom = 1.0f,
+    };
+
+    Waves waves = {0};
+    {
+        nob_da_append(&waves, (AWave)make_wave(wave_1_enemies));
+        nob_da_append(&waves, (AWave)make_wave(wave_2_enemies));
+        nob_da_append(&waves, (AWave)make_wave(wave_3_enemies));
+    };
+
+    Nob_String_Builder sb = {0};
 
     while (!WindowShouldClose())
     {
-
         float dt = GetFrameTime();
 
         Vector2 direction = {0};
@@ -82,13 +153,50 @@ int main(void)
         else if (IsKeyDown(KEY_A))
             direction.x = -speed * dt;
 
-        player_move(&player, controller_options, direction);
+        player_move(&controller_options, direction);
+
+        AWave *wave = &waves.items[waves.current];
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            nob_da_foreach(Enemy, it, wave)
+            {
+                if (it->health > 0)
+                {
+                    it->health = 0;
+                    break;
+                }
+            }
+        }
+
+        if (is_wave_over(wave))
+        {
+            waves.current++;
+        }
 
         camera.target = (Vector2){player.x + player.width / 2.0f, player.y + player.height / 2.0f};
 
         BeginDrawing();
         {
             ClearBackground(BLACK);
+
+            sb.count = 0;
+            nob_sb_appendf(&sb, "Wave: %zu\n", waves.current);
+
+            nob_da_foreach(Enemy, it, wave)
+            {
+                if (it->health > 0)
+                {
+                    nob_sb_appendf(&sb, "Enemy: %zu\n", it->health);
+                }
+                else
+                {
+                    nob_sb_appendf(&sb, "Enemy: <dead>\n");
+                }
+            }
+
+            nob_sb_append_null(&sb);
+            DrawText(sb.items, 100, 100, 15, WHITE);
 
             BeginMode2D(camera);
             {
@@ -98,5 +206,7 @@ int main(void)
             EndMode2D();
         }
         EndDrawing();
+
+        nob_temp_reset();
     }
 }
